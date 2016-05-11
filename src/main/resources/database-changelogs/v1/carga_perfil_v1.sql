64f40ebf513b9be32e9adc08035ff24e8d0e08b7
mirandarfsm <harripobre@gmail.com>
2016-05-10 23:50:07 -0300

    Adicionado Tela de cadastro de usuario

--- src/main/resources/database-changelogs/v1/carga_perfil_v1.sql
+++ src/main/resources/database-changelogs/v1/carga_perfil_v1.sql
@@ -1,2 +1,3 @@
 INSERT INTO tb_perfil (id_perfil,NOME) VALUES ('ROLE_USER','Usuário');
+INSERT INTO tb_perfil (id_perfil,NOME) VALUES ('ROLE_FUNC','Funcionário da Empresa');
 INSERT INTO tb_perfil (id_perfil,NOME) VALUES ('ROLE_ADMIN','Administrador do Sistema');
